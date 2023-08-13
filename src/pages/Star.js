107351ff3ae0f1459a3726d238da87c25f56ae41
swchoi00 <celpic-@naver.com>
2023-08-13 22:59:35 +0900

    '등록기능'

--- src/pages/Star.js
+++ src/pages/Star.js
@@ -6,29 +6,35 @@ import './Star.css';
 const Star = () => {
 
   const count = [0, 1, 2, 3, 4]
-  const [clicked, setClicked] = useState([false, false, false, false, false]) 
+  const [clicked, setClicked] = useState([false, false, false, false, false])
 
   const handleStarClick = index => {
     let clickStates = [...clicked];
     for (let i = 0; i < 5; i++) {
-    clickStates[i] = i <= index ? true : false;
+      clickStates[i] = i <= index ? true : false;
+      console.log(clicked)
     }
-  setClicked(clickStates);
+    setClicked(clickStates);
   };
 
-  // useEffect(() => {
-  //   sendReview();
-  // }, [clicked])
+  useEffect(() => {
+    sendReview();
+  }, [clicked]);
+
+  const sendReview = () => {
+    let score = clicked.filter(Boolean).length;
+  }
 
 
-
-return (
-  count.map((data) => (
-    <ImStarFull key={data} onClick={() => handleStarClick(data)}
-      className= { clicked[data] && 'yellowStar' }
-      size= '100'
-  />))
+  return (
   
+    count.map((data, i) => (
+      <ImStarFull key={i} onClick={() => handleStarClick(data)}
+        className={clicked[data] && 'yellowStar'}
+        size='100'
+        />))
+       
+
   )
 }
 
--- src/pages/Write.js
+++ src/pages/Write.js
@@ -15,11 +15,10 @@ const Write = () => {
   const [chooseLocation, setLocation] = useState();
 
   const [info, setInfo] = useState({
-    // location : '',
-    // name : '',
-    // file : '',
-    // review : ''
-    name : ''
+    name : '',
+    location : '',
+    file : '',
+    review : '',
 
   });
 
@@ -28,13 +27,43 @@ const Write = () => {
   // 수정중
   const submitInfoHandler = (e) => {
     console.log(info)
+    
+    if(window.confirm('등록하시겠습니까?')) 
+      navigate(-1);
+    
   }
 
-  const onChangeHandler = (e) => {
-    setLocation(e.target.value);
-  }
+  const onChangeNameHandler = (e) => {
+    setInfo((saveInfo) => ({
+      ...saveInfo,
+      name : e.target.value,
+    }));
+  };
+
+  const onChangeLocationHandler = (e) => {
+    setInfo((saveInfo) => ({
+      ...saveInfo,
+      location : e.target.value,
+    }));
+  };
+
+  const onChangeFileHandler = (e) => {
+    setInfo((saveInfo) => ({
+      ...saveInfo,
+      file : e.target.value,
+    }));
+  };
+
+  const onChangeReviewHandler = (e) => {
+    setInfo((saveInfo) => ({
+      ...saveInfo,
+      review : e.target.value,
+    }));
+  };
+
 
   const location = [
+    {value : 'default', name : '지역선택'},
     {value : 'gangseo', name : '강서구'},
     {value : 'gangdong', name : '강동구'},
     {value : 'gangnam', name : '강남구'},
@@ -44,11 +73,9 @@ const Write = () => {
   // const [chooseLocation, setChooseLocation] = useState('gangseo');
 
   const goBack = () => {
-    if(window.confirm('정말 취소하시겠습니까?')) {
+    if(window.confirm('정말 취소하시겠습니까?')) 
       navigate(-1);
-    } else {
-      
-    }
+
   }
 
   return (
@@ -60,17 +87,17 @@ const Write = () => {
 
     <div>
       <h3>맛집 이름</h3>
-      <input className="restName" type="text" style={{}} placeholder="맛집의 이름을 알려주세요"></input>
+      <input className="restName" type="text" style={{}} placeholder="맛집의 이름을 알려주세요" onChange={onChangeNameHandler}></input>
     </div>
 
 
     <div>
       <h3>지역 선택</h3>
-      <Form.Select aria-label="Default select example" onChange={onChangeHandler} value={chooseLocation}>
+      <Form.Select aria-label="Default select example" onChange={onChangeLocationHandler} value={chooseLocation} >
         {
           location.map((data, i) => {
             return (
-              <option key={i} value={data.value}>
+              <option key={i} value={data.value} selected={data.value === 'default'} disabled={data.value === 'default'} >
                 {data.name}
               </option>
             );
@@ -80,12 +107,12 @@ const Write = () => {
 
       <div>
         <h3>사진 첨부</h3>
-        <input type="file"></input>
+        <input type="file" onChange={onChangeFileHandler}></input>
       </div>
 
       <div>
         <h3>리뷰 작성</h3>
-        <textarea  className="reviewArea" placeholder="리뷰를 작성해주세요"></textarea>
+        <textarea  className="reviewArea" placeholder="리뷰를 작성해주세요" onChange={onChangeReviewHandler}></textarea>
       </div>
 
       <div className="Rate">
